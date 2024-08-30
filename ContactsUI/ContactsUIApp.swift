39a1aa490b4db3ca00c757d0c6d01e97c0484af2
Sheen <sheenvempeny@gmail.com>
2024-08-30 14:13:26 -0500

    Initial Commit

---
+++ ContactsUI/ContactsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsUIApp.swift
+//  ContactsUI
+//
+//  Created by Sheen on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
